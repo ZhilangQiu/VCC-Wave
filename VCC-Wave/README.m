11de032e5d5d693e05f6aa1cdf8560d96273d459
ZhilangQiu <2531876783@qq.com>
2021-02-02 17:45:50 +0800

    Add files via upload

---
+++ VCC-Wave/README.m
@@ -0,0 +1,42 @@
+
+
+% =====================================================================================
+% References
+% =====================================================================================
+Matlab source code of all reconstructions to reproduce the results as described in:
+
+Qiu Z, Jia S, Su S, Zhu Y, Liu X, Zheng H, Wang H, Liang D.
+“Combining virtual conjugate coil reconstruction and wave encoding for improved parallel MRI”
+Magn Reson Med. 2021;
+
+
+% =====================================================================================
+% Demos
+% =====================================================================================
+demo_Fig4: source code to reproduce Figure 4 
+
+demo_Fig6: source code to reproduce Figure 6
+
+demo_Fig8: source code to reproduce Figure 8
+
+
+% =====================================================================================
+% Demos
+% =====================================================================================
+The source code is implemented or modified based on the following external packages:
+
+- Wave-CAIPI toolbox by Berkin Bilgic
+ (http://martinos.org/~berkin/software.html) 
+ 
+- ESPIRiT codes by Michael Lustig
+ (https://people.eecs.berkeley.edu/~mlustig/Software.html) 
+
+- Parallel imaging reconstruction codes by Michael S. Hansen
+ (http://hansenms.github.io/sunrise) 
+ 
+
+% =====================================================================================
+% Contact info
+% =====================================================================================
+(c) Zhilang Qiu, 2021, Shenzhen Institutes of Advanced Technology, Chinese Academy of Sciences
+Email: zhilangqiu@gmail.com / zl.qiu@siat.ac.cn
